7107ca4cf8f67fd1c03d73ddb2fe630a8c2d5c01
Leo Lei <leolei@berkeley.edu>
2018-04-07 17:53:33 +0800

    please wait button after clicking send

--- src/components/forms/EmailForm.js
+++ src/components/forms/EmailForm.js
@@ -4,13 +4,14 @@ import { Alert, Button, Fade, Form, FormGroup, Label, Input } from 'reactstrap'
 class EmailForm extends Component {
   constructor(props) {
     super(props)
-    this.state = {error: '', recipientEmail: '', message: '', subject: '', emailSent: false}
+    this.state = {error: '', recipientEmail: '', message: '', subject: '', emailSending: false, emailSent: false}
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleFieldChange = this.handleFieldChange.bind(this)
     this.resetStates = this.resetStates.bind(this)
   }
 
   async handleSubmit() {
+    this.setState({emailSending: true})
     const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/emails`, {
       method: 'POST',
       headers: {
@@ -26,13 +27,14 @@ class EmailForm extends Component {
     })
     const jsonResponse = await response.json()
     if (response.status === 400) {
-      this.setState({error: jsonResponse.error})
+      this.setState({emailSending: false, error: jsonResponse.error})
     } else if (!response.ok) {
       this.setState({
+        emailSending: false,
         error: `Email could not be delivered. We have tried ${jsonResponse.tryCount} time(s).`
       })
     } else {
-      this.setState({emailSent: true})
+      this.setState({emailSent: true, emailSending: false})
     }
   }
 
@@ -46,6 +48,7 @@ class EmailForm extends Component {
       recipientEmail: '',
       message: '',
       subject: '',
+      emailSending: false,
       emailSent: false
     })
   }
@@ -87,7 +90,7 @@ class EmailForm extends Component {
             name="message"
           />
         </FormGroup>
-        <Button color="primary" onClick={this.handleSubmit}>Submit</Button>
+        <Button color="primary" disabled={this.state.emailSending} onClick={this.handleSubmit}>{this.state.emailSending ? 'Please Wait...' : 'Submit'}</Button>
       </Form>
     )
   }
